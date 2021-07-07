b51feebce6af0dd48146f83d5d71cfc12cc7413e
loiseupro <86427844+loiseupro@users.noreply.github.com>
2021-07-07 13:57:44 +0200

    Update fibonacci.js

--- fibonacci.js
+++ fibonacci.js
@@ -1,15 +1,15 @@
 function fibonacci(limit){
-			var sequence = [0,1];		
+	var sequence = [0,1];		
+	
+	if(limit == 0) sequence = [0];
+	if(limit > 1) sequence = [0, 1];
+	for(i=2; i<limit; i++) sequence.push(sequence[i-1] + sequence[i-2]);  
 
-		    if(limit == 0) sequence = [0];
-		    if(limit > 1) sequence = [0, 1];
-		    for(i=2; i<limit; i++) sequence.push(sequence[i-1] + sequence[i-2]);  
+	return sequence;
+}
 
-		    return sequence;
-		}
-		
-		// Test
-		serie = fibonacci(10);
-		for(i=0; i < serie.length; i++){
-			document.write(serie[i]+"<br/>");	
-		}
+// Test
+serie = fibonacci(10);
+for(i=0; i < serie.length; i++){
+	document.write(serie[i]+"<br/>");	
+}
